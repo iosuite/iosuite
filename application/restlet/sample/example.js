6be281e5a6e1afca25eb37904b7360cc1a673d57
TJ Tyrrell <innovate@tjtyrrell.com>
2015-03-27 15:45:11 -0600

    Sample restlet call

--- application/restlet/sample/example.js
+++ application/restlet/sample/example.js
@@ -1,11 +1,37 @@
 (function(){
 
-	var private = {},
-		public = {};
+	var _public = {},
+		_private = {};
 
-	public.readCall = function( dataIn ) {
-		nlapiLogExecution('DEBUG','sample ran','This Sample was successfully called');
-		return {};
+	_public.readCall = function( dataIn ) {
+		
+		var returnPayload = {};
+
+		private._addExecutionTime( 'startRestletCall' );
+
+		if( dataIn.error ) {
+
+			/*
+			 *  Sample Error
+			 */
+			private._setStatus( 'ERROR' );
+			private._addMessage( 400, dataIn.error );
+
+		} else {
+
+			private._addExecutionTime( 'executeRestlet' );
+			nlapiLogExecution('DEBUG','executeRestlet','Example NLAPI call');
+
+			returnPayload.example = 'Sample payload return data';
+
+		}
+
+		if( private._getStatus != 'ERROR' ) {
+			private._setStatus( 'SUCCESS' );
+		}
+
+		private._setReturnPayload( returnPayload );
+
 	}
 
 	return public;
