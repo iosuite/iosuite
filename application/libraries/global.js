127d95fd81f50a0b1125f03c02103e0c5aea72cc
TJ Tyrrell <innovate@tjtyrrell.com>
2015-04-20 14:12:33 -0600

    Added global error handler

--- application/libraries/global.js
+++ application/libraries/global.js
@@ -22,6 +22,39 @@
 		return data;
 	};
 
+	public.handleError = function( error, type ) {
+
+		var responseError = {};
+
+		if(error instanceof nlobjError ) {
+
+			responseError.code = error.getCode();
+			responseError.details = error.getDetails();
+			responseError.lineNum = 'Unknown';
+			responseError.stack = error.getStackTrace();
+			responseError.assocRecord = error.getInternalId();
+			responseError.misc = error.getUserEvent();
+			
+		} else {
+			
+			responseError.code = error.name;
+			responseError.details = error.message;
+			responseError.lineNum = error.lineNumber;
+			var stackString = error.stack;
+			responseError.assocRecord = 'Unknown';
+			responseError.misc = error.rhinoException;
+			
+			responseError.stack = stackString.split('at');
+			
+		}
+
+		for( var item in responseError ) {
+			nlapiLogExecution('ERROR', type, responseError[item]);
+		}
+
+		return responseError;
+	}
+
 	return public;
 
 })()
